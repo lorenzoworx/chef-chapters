7afe12166ccec4bd3bbff0484eb0cbffdeb5a6f5
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-19 16:13:21 +0100

    Fix naming convention

--- db/migrate/20230919151038_create_recipe_food.rb
+++ db/migrate/20230919151038_create_recipe_food.rb
@@ -1,6 +1,6 @@
 class CreateRecipeFood < ActiveRecord::Migration[7.0]
   def change
-    create_table :recipe_foods do |t|
+    create_table :recipe_food do |t|
       t.integer :quantity
       t.references :recipe, foreign_key: { to_table: :recipes }
       t.references :food, foreign_key: { to_table: :food }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_150137) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_151038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,14 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_150137) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "recipe_foods", force: :cascade do |t|
+  create_table "recipe_food", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "recipe_id"
     t.bigint "food_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
-    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+    t.index ["food_id"], name: "index_recipe_food_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_food_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_150137) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "recipe_foods", "food"
-  add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipe_food", "food"
+  add_foreign_key "recipe_food", "recipes"
   add_foreign_key "recipes", "users"
 end
