9f770ad440d74e104a9f00d50c50b5cae0982710
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-20 12:22:20 +0300

    Add routes for each page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
+  root to: 'recipes#index'
+  get '/recipes/my_recipes', to: 'recipes#my_recipes'
+  get '/foods', to: 'foods#index'
+  get '/inventories', to: 'inventories#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
