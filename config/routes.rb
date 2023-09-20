0571ea2f669af62264f9e303da6573dec46cd4c5
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-20 17:07:50 +0300

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   get '/recipes/my_recipes', to: 'recipes#my_recipes'
   get '/foods', to: 'foods#index'
   get '/inventories', to: 'inventories#index'
-
+  get '/inventories', to: 'inventories#index'
+  post '/inventories/create', to: 'inventories#create'
+  post '/inventories/destroy', to: 'inventories#destroy'
+  post 'inventories/:inventory_id/add', to: 'inventories#add'
+  post 'inventories/:inventory_id/remove/:inv_fod_id', to: 'inventories#remove'
+  get 'inventories/:inventory_id', to: 'inventories#show'
+  get '/inventories/compare/:recipes_id/:inventory_id', to: 'inventories#compare'
   # Defines the root path route ("/")
   # root "articles#index"
 end
