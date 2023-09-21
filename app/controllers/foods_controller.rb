4db6e9bda50cabc3107b4e8816b78bd117c39e68
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-21 12:36:21 +0100

    Add index, new create and destroy actions to FoodsController

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,30 @@
 class FoodsController < ApplicationController
+    def index
+        @foods = Food.all
+    end
+    def new
+        @food = Food.new
+      end
+    
+      def create
+        @food = Food.new(food_params)
+      
+        if @food.save
+          redirect_to foods_path
+        else
+          render :new
+        end
+      end
+
+      def destroy
+        @food = Food.find(params[:id])
+        @food.destroy
+        redirect_to foods_path, notice: "Food item was successfully deleted."
+      end      
+    
+      private
+    
+      def food_params
+        params.require(:food).permit(:name, :measurement_unit, :price)
+      end
 end
