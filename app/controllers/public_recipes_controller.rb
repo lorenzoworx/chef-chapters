d16fbc44b9a6bdc317c3d745191ba39b1f2318a3
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-21 16:51:27 +0300

    fix linters error

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PublicRecipesController < ApplicationController
   def index
     @public_recipes = Recipe.where(public: true)
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Food < ApplicationRecord
   has_many :recipe_food
   has_many :recipes, through: :recipe_food
