bcbc765f9f34ebcf661d6733080e5e6a46872d09
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-21 12:44:02 +0100

    Add index, new create and destroy actions to RecipesController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,49 @@
 class RecipesController < ApplicationController
-  def my_recipes; end
+  before_action :authenticate_user!
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      flash[:success] = "Recipe created successfully!"
+      redirect_to @recipe # Redirect to the newly created recipe's show page
+    else
+      render 'new' # Render the 'new' view template to display validation errors
+    end
+  end
+
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      flash[:success] = "Recipe deleted successfully!"
+    else
+      flash[:error] = "An error occurred while deleting the recipe."
+    end
+    redirect_to recipes_path
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  private
+
+  def find_recipe
+    @recipe = current_user.recipes.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = "Recipe not found."
+    redirect_to recipes_path
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
