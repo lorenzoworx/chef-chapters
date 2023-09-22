d840f045450fdc097ae6a9fddb400a8e9c77699f
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-22 10:33:50 +0300

    Update the models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
-
 class Food < ApplicationRecord
-  has_many :recipe_food
-  has_many :recipes, through: :recipe_food
+  has_many :recipe_foods, foreign_key: :food_id
+  belongs_to :user
+  validates :name, :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { only_decimal: true, greater_than: 0 }
 end
