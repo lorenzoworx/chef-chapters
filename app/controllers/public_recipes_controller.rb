436e0d4ee0d266415a44e5240163b3d11ab0c51f
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-09-22 23:39:39 +0300

    display all public recipes for all users

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class PublicRecipesController < ApplicationController
-  def index
-    @recipes = current_user.recipes.includes(:recipe_foods).where(public: true)
+  def index 
+    @recipes = Recipe.includes(:recipe_foods).where(public: true)
     @recipe_foods = RecipeFood.joins(:food, :recipe).where(food: { user: current_user }, recipe: { user: current_user })
     @total_cost = @recipe_foods.sum('quantity*price')
     @total_items = @recipe_foods.select('food_id').distinct.count
