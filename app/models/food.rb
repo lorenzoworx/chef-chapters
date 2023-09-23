0e8841f87f379849d89aefecca48fe43a5e6eb47
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-23 01:01:00 +0100

    Update food model to include dependent destroy

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Food < ApplicationRecord
-  has_many :recipe_foods, foreign_key: :food_id
+  has_many :recipe_foods, foreign_key: :food_id, dependent: :destroy
   belongs_to :user
   validates :name, :measurement_unit, presence: true
   validates :price, presence: true, numericality: { only_decimal: true, greater_than: 0 }
