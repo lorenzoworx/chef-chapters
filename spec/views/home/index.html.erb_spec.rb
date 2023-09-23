73ea7fbcf9dc03218f0bfa87fcfedff36986aae5
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-23 01:13:29 +0100

    Create spec for home index view

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,3 +1,12 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
+
+RSpec.feature 'Welcome Page', type: :feature do
+  scenario 'displays welcome message and links' do
+    visit root_path
+
+    expect(page).to have_content('Welcome to Chef Chapters')
+    expect(page).to have_content('Join our community of chefs and food enthusiasts!')
+    expect(page).to have_link('Sign up', href: new_user_registration_path)
+    expect(page).to have_link('Log in', href: new_user_session_path)
+  end
+end
